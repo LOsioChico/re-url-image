222003ce9e72a9533bc0028b7a4123142b62dd52
Luis Osio Chico <72286405+LOsioChico@users.noreply.github.com>
2024-02-12 17:47:47 -0500

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,11 @@
 import { Hono } from "hono";
+import { cors } from "hono/cors";
 import { Bindings } from "hono/types";
 import { getTransformedCloudinaryURL, uploadImageToCloudinary } from "./service";
 import { zUploadRequestValidator } from "./validator";
 
 const app = new Hono<{ Bindings: Bindings }>();
+app.use(cors());
 
 app.post("/upload", zUploadRequestValidator, async (c) => {
   const { url } = await uploadImageToCloudinary(c);
